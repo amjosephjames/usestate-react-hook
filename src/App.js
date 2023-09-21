599158ef465d0b49ac44e14dbe662524a460640d
amjosephjames <amjoseph4231@gmail.com>
2023-09-21 01:52:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Homescreen from "../src/components/homescreen";
+
+const app = () => {
+  return (
+    <div>
+      {" "}
+      <Homescreen />{" "}
+    </div>
+  );
+};
+export default app;
